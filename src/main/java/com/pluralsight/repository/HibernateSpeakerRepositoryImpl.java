300f33fcc20c68d6a9f900c9568a27d67e747db6
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-11-29 16:55:11 +0100

    First commit

---
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Override
+    public List<Speaker> findAll() {
+        List<Speaker> speakers=new ArrayList<>();
+
+        Speaker speaker=new Speaker();
+
+        speaker.setFirstName("Nemanja");
+        speaker.setLastName("Mitrovic");
+
+        speakers.add(speaker);
+
+        return speakers;
+    }
+
+}
